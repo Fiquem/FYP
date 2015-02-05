b992cdb035b23db1602144b86ce63c5202929250
Emma Carrigan <emmeridge@hotmail.com>
2015-02-05 22:45:10 +0000

    CAMERA SO GOOD

--- js/camera.js
+++ js/camera.js
@@ -6,27 +6,24 @@ var cam_orient = [0.0,0.0,1.0,0.0];
 var right = [1,0,0];
 var up = [0,1,0];
 var forward = [0,0,-1];
-
-// QUAT CODE
-var axis = inverse_mat4(quat_to_mat4(cam_orient));
-var view_mat = mult_mat4_mat4 (
-	quat_to_mat4 (cam_orient), 
-	translate_mat4 (identity_mat4(), [-cam_pos[0], -cam_pos[1], -cam_pos[2]])
-	);
+var pitch = 0;
+var yaw = 180;
 
 function update_cam() {
     axis = inverse_mat4 (quat_to_mat4 (cam_orient));
     right = mult_mat4_mat4 (axis, [1.0, 0.0, 0.0, 0.0]);
-    up = mult_mat4_mat4 (axis, [0.0, 1.0, 0.0, 0.0]);
     forward = mult_mat4_mat4 (axis, [0.0, 0.0, -1.0, 0.0]);
 
     // constrain camera
     right[1] = 0.0;
-    up = [0.0,1.0,0.0];
     forward[1] = 0.0;
 
+    // yaw * pitch * translation
     view_mat = mult_mat4_mat4 (
-	quat_to_mat4 (cam_orient), 
+	mult_mat4_mat4 (
+		rotate_x_deg (identity_mat4 (), pitch),
+		rotate_y_deg (identity_mat4 (), yaw)
+		),
 	translate_mat4 (identity_mat4(), [-cam_pos[0], -cam_pos[1], -cam_pos[2]])
 	);
 }
--- js/input.js
+++ js/input.js
@@ -17,10 +17,11 @@ function handleMouseMovement(event) {
     if (keys_down[16])
     {
         yaw += (x - last_x)*speed;
-        pitch += (y - last_y)*speed;
+        if ((pitch < 20 && (y - last_y) > 0) || (pitch > -20 && (y - last_y) < 0)) pitch += (y - last_y)*speed;
+        cam_orient = mult_quat_quat (quat_from_axis_deg ((x - last_x)*speed, up[0], up[1], up[2]), cam_orient);
+        cam_orient = mult_quat_quat (quat_from_axis_deg ((y - last_y)*speed, right[0], right[1], right[2]), cam_orient);
     }
     if (yaw > 360) yaw = 0;
-    if (pitch > 360) pitch = 0;
     last_x = x;
     last_y = y;
 }
@@ -28,12 +29,16 @@ function handleMouseMovement(event) {
 function handleKeys() {
     // WASD - cam move
     if (keys_down[87])
+        //cam_pos[2] -= speed;
         move(cam_pos, forward, 1, 0.1);
     if (keys_down[65])
+        //cam_pos[0] -= speed;
         move(cam_pos, right, -1, 0.1);
     if (keys_down[83])
+        //cam_pos[2] += speed;
         move(cam_pos, forward, -1, 0.1);
     if (keys_down[68])
+        //cam_pos[0] += speed;
         move(cam_pos, right, 1, 0.1);
 }
 
