c4bf860eaee3d795c73a5f745ef68410e94a8b08
Emma Carrigan <emmeridge@hotmail.com>
2015-02-06 21:01:40 +0000

    camera tooooootally fixed I HOPE

--- js/input.js
+++ js/input.js
@@ -19,7 +19,6 @@ function handleMouseMovement(event) {
         yaw += (x - last_x)*speed;
         if ((pitch < 20 && (y - last_y) > 0) || (pitch > -20 && (y - last_y) < 0)) pitch += (y - last_y)*speed;
         cam_orient = mult_quat_quat (quat_from_axis_deg ((x - last_x)*speed, up[0], up[1], up[2]), cam_orient);
-        cam_orient = mult_quat_quat (quat_from_axis_deg ((y - last_y)*speed, right[0], right[1], right[2]), cam_orient);
     }
     if (yaw > 360) yaw = 0;
     last_x = x;
@@ -29,16 +28,12 @@ function handleMouseMovement(event) {
 function handleKeys() {
     // WASD - cam move
     if (keys_down[87])
-        //cam_pos[2] -= speed;
         move(cam_pos, forward, 1, 0.1);
     if (keys_down[65])
-        //cam_pos[0] -= speed;
         move(cam_pos, right, -1, 0.1);
     if (keys_down[83])
-        //cam_pos[2] += speed;
         move(cam_pos, forward, -1, 0.1);
     if (keys_down[68])
-        //cam_pos[0] += speed;
         move(cam_pos, right, 1, 0.1);
 }
 
